56f9351f2d382c4ef4ecd3a43998c5b120c7db4d
Louis Boyd <louis.d.boyd@gmail.com>
2020-10-08 01:46:24 +0100

    Added SSLVPN and Inspection Policy

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,7 @@
 import base64
 from uuid import uuid4
 
+
 def convertStringToBase64(string):
 
     string_bytes = string.encode('ascii')
@@ -8,11 +9,13 @@ def convertStringToBase64(string):
     string_base64 = base64_bytes.decode('ascii')
     return string_base64
 
+
 def convertFileToBase64(filename):
     with open(filename, 'r') as certificateFile:
         file_base64 = convertStringToBase64(certificateFile.read())
     return file_base64
 
+
 def generateUniqueCertName():
-    unique_certificate_name = uuid4().hex 
+    unique_certificate_name = uuid4().hex
     return unique_certificate_name
--- sample.py
+++ sample.py
@@ -1,12 +1,22 @@
+import os
 from fortigate_automation import Fortigate
 from helper_functions import convertFileToBase64, generateUniqueCertName
 
+backupDir = os.path.abspath(os.getcwd() + "/config_backups")
 sample60e = Fortigate("https://192.168.0.1:4443", "API_KEY", False)
 
 certificate = convertFileToBase64("certificate.crt")
 key = convertFileToBase64("key.key")
 certificate_name = generateUniqueCertName()
 
-sample60e.output_details()
-sample60e.backupConfig()
-sample60e.uploadCertificate(certificate_name, certificate, key)
+sample60e.outputDetails()
+backup = sample60e.backupConfig()
+sample60e.uploadLocalCertificate(certificate_name, certificate, key)
+sample60e.setSSLVPNCertificate(certificate_name)
+sample60e.setInspectionProfileServerCertificate("endpoint-certificate", certificate_name)
+
+if not os.path.exists(backupDir):
+    os.makedirs(backupDir)
+
+with open(backupDir + "/" + backup['original_filename'], 'w') as configBackupWriter:
+    configBackupWriter.write(backup['configuration'])
